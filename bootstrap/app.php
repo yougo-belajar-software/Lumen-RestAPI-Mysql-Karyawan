e5eef4cbc869428daf264c0a606dc978788fc5e2
ALi Home PC <admin@wabatam.com>
2020-03-27 15:56:23 +0700

    cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -80,6 +80,9 @@ $app->configure('app');
 //     'auth' => App\Http\Middleware\Authenticate::class,
 // ]);
 
+    $app->middleware([
+        App\Http\Middleware\CorsMiddleware::class
+    ]);
 /*
 |--------------------------------------------------------------------------
 | Register Service Providers
